57785b43bd6d456612aec2e9c4180d89ad748b4a
jperelygin <jperelygin@gmail.com>
2019-03-19 16:14:38 +0300

    init commit

---
+++ ds_mailer/mailer/Sender.py
@@ -0,0 +1,31 @@
+import Sitescanner
+import Result
+import creds
+import smtplib, ssl
+
+
+def send_email(text):
+    port = 465 # for SSL
+    password = creds.PASSWORD
+    sender = creds.EMAIL
+    receiver =  creds.RECEIVER
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender, password)
+        print(text)
+        server.sendmail(sender, receiver, msg=text.encode('utf-8')) 
+
+
+if __name__ == "__main__":
+    scanner = Sitescanner.Sitecanner()
+    prices = []
+    for i in scanner.URLS:
+        prices.append(scanner.get_price(i))
+    try:
+        result = Result.Result(prices[0],prices[1],prices[2],prices[3])
+    except Exception:
+        print(Exception)
+    letter = result.make_letter_text()
+    send_email(letter)
---
+++ ds_mailer/mailer/Sitescanner.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+from lxml import html
+
+class Sitecanner:
+
+    URLS = [
+    "https://store.playstation.com/ru-ru/product/EP0700-CUSA08495_00-DARKSOULSHD00000", # ds1_url
+    "https://store.playstation.com/ru-ru/product/EP0700-CUSA01589_00-DARKSOULS2000003", # DARK SOULS 2 PS4 URL
+    "https://store.playstation.com/ru-ru/product/EP0700-CUSA03365_00-DSIIIDELUXE00000", # DARK SOULS 3 DELUXE URL
+    "https://store.playstation.com/ru-ru/product/EP9000-CUSA03173_00-BLOODBORNE0000EU" # Bloodbourne game of th year
+    ]
+
+    def get_price(self, url):
+        r = requests.get(url)
+        page = r.text
+        soup = BeautifulSoup(page, features="lxml")
+        price = soup.find("h3", {"class":"price-display__price"})
+        return price.text
+        
