ac280256d3b50589feca80cd2a0c539d5115fafb
jperelygin <jperelygin@gmail.com>
2019-03-21 13:03:44 +0300

    refactoring of the GAMES list. Game.py added

--- ds_mailer/mailer/Sender.py
+++ ds_mailer/mailer/Sender.py
@@ -1,30 +1,32 @@
 import Sitescanner
-import Result
+from Sitescanner import GAMES, TEST_GAMES
 import creds
 import smtplib, ssl
+import datetime
 
+def create_mail(games_arr):
+    date = datetime.date.today().isoformat()
+    subject = f"Subject: {date} FROM SOFTWARE PRICES\n"
+    text = subject
+    for j in games_arr:
+        text += f"\n{j.name}:\t{j.usual_price}\n"
+    return text
 
 def send_email(text):
     port = 465 # for SSL
     password = creds.PASSWORD
     sender = creds.EMAIL
-    receiver =  creds.RECEIVER
-
+    reciever = creds.RECIEVER
     context = ssl.create_default_context()
-
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(sender, password)
-        server.sendmail(sender, receiver, msg=text.encode('utf-8')) 
+        server.sendmail(sender, reciever, msg=text.encode('utf-8')) 
 
 
 if __name__ == "__main__":
     scanner = Sitescanner.Sitecanner()
-    prices = []
-    for i in creds.GAMES:
-        prices.append(scanner.get_price(creds.GAMES[i]))
-    try:
-        result = Result.Result(prices[0],prices[1],prices[2],prices[3])
-    except Exception:
-        print(Exception)
-    letter = result.make_letter_text()
+    for i in TEST_GAMES:
+        #prices.append(scanner.get_price(i.link))
+        i.get_current_price(scanner.get_price(i.link))
+    letter = create_mail(TEST_GAMES)
     send_email(letter)
--- ds_mailer/mailer/Sitescanner.py
+++ ds_mailer/mailer/Sitescanner.py
@@ -1,16 +1,38 @@
 from bs4 import BeautifulSoup
 import requests
-from lxml import html
+from Game import Game
+
+GAMES = [
+    Game("DARK SOULS 1. PS4 Remastered",
+     "https://store.playstation.com/ru-ru/product/EP0700-CUSA08495_00-DARKSOULSHD00000", 
+     "2.599"),
+    Game("DARK SOULS 2. PS4 Scholar of the first sin",
+     "https://store.playstation.com/ru-ru/product/EP0700-CUSA01589_00-DARKSOULS2000003",
+     "1.199"),
+    Game("DARK SOULS 3. PS4 Delux Edition",
+     "https://store.playstation.com/ru-ru/product/EP0700-CUSA03365_00-DSIIIDELUXE00000",
+      "3.999"),
+    Game("Bloodbourne. PS4 Game of the year",
+     "https://store.playstation.com/ru-ru/product/EP9000-CUSA03173_00-BLOODBORNE0000EU",
+     "1.999")
+]
+
+TEST_GAMES = [
+    Game("DARK SOULS 1. PS4 Remastered", 
+     "https://store.playstation.com/ru-ru/product/EP0700-CUSA08495_00-DARKSOULSHD00000", 
+     "2.699"),
+    Game("DARK SOULS 2. PS4 Scholar of the first sin",
+     "https://store.playstation.com/ru-ru/product/EP0700-CUSA01589_00-DARKSOULS2000003",
+     "1.199"),
+    Game("DARK SOULS 3. PS4 Delux Edition",
+     "https://store.playstation.com/ru-ru/product/EP0700-CUSA03365_00-DSIIIDELUXE00000",
+     "4.999"),
+    Game("Bloodbourne. PS4 Game of the year",
+     "https://store.playstation.com/ru-ru/product/EP9000-CUSA03173_00-BLOODBORNE0000EU",
+     "1.999")
+]
 
 class Sitecanner:
-
-    # an old realisation 
-    #URLS = [
-    #"https://store.playstation.com/ru-ru/product/EP0700-CUSA08495_00-DARKSOULSHD00000", # ds1_url
-    #"https://store.playstation.com/ru-ru/product/EP0700-CUSA01589_00-DARKSOULS2000003", # DARK SOULS 2 PS4 URL
-    #"https://store.playstation.com/ru-ru/product/EP0700-CUSA03365_00-DSIIIDELUXE00000", # DARK SOULS 3 DELUXE URL
-    #"https://store.playstation.com/ru-ru/product/EP9000-CUSA03173_00-BLOODBORNE0000EU" # Bloodbourne game of th year
-    #]
 
     def get_price(self, url):
         r = requests.get(url)
