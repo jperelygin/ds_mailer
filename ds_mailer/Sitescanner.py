85473ed4a095edcd6c55ab2a5c30b6a4f0461574
jperelygin <jperelygin@gmail.com>
2020-09-25 19:14:34 +0300

    Add Sitescanner retries if price is NoneType

--- ds_mailer/Sitescanner.py
+++ ds_mailer/Sitescanner.py
@@ -1,12 +1,24 @@
+import time
 from bs4 import BeautifulSoup
 import requests
 
 
+NUMBER_OF_RETRIES = 5
+DELAY_BETWEEN_TRIES = 5  # seconds
+
+
 class Sitescanner:
+
+    def __init__(self):
+        self.retries = 0
 
     def get_price(self, url):
         r = requests.get(url)
         page = r.text
         soup = BeautifulSoup(page, features="lxml")
         price = soup.find("h3", {"class": "price-display__price"})
+        if not price.text and self.retries < NUMBER_OF_RETRIES:
+            self.retries += 1
+            time.sleep(DELAY_BETWEEN_TRIES)
+            self.get_price(url)
         return price.text
